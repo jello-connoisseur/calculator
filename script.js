9a969f1259055540926e59705344fd23847d0879
Jewon Hwang <jewon.hwang@gmail.com>
2021-11-22 17:57:57 -0500

    calculator works,but need to input one extra operation at the beginning

--- script.js
+++ script.js
@@ -10,33 +10,63 @@ const multiply = (a, b) => a * b;
 //divide
 const divide = (a, b) => a / b;
 
-function operate(operator, a, b){
+function operate(a, b, operator){
     if (operator === "+"){
         return add(a,b);
     }
     else if (operator === "-"){
         return subtract(a,b);
     }
-    else if (operator === "*"){
+    else if (operator === "x"){
         return multiply(a,b);
     }
-    else if (operator === "%"){
+    else if (operator === "÷"){
         return divide(a,b);
     } else return "not valid operator";
 }
 
-const btn = document.querySelectorAll('.numbers');
+const btn = document.querySelectorAll('.num');
 const output = document.querySelector('.output');
-const content = document.createElement('div');
+let content = document.createElement('div');
 content.classList.add('content');
+let array = [];
+
 
 btn.forEach((button) => {
     button.addEventListener('click',(e) =>{
         content.textContent += e.target.textContent;
         output.appendChild(content);
-
-    })
+    });
 })
-function display(){
 
-}
+
+const op = document.querySelectorAll('.operator');
+let calculate = "";
+op.forEach((operator) => {
+    operator.addEventListener('click', (e) => {
+        //store the operator chosen
+        calculate = e.target.textContent;
+        //store the desired number in the array
+        array.push(content.textContent);
+        //clear window and reset number
+        content.textContent = '';
+        console.log(array);
+        // console.log(operate(displayedContent, e.target.textContent, displayedContent));
+
+    });
+})
+
+const equals = document.querySelector('.equals');
+
+equals.addEventListener('click', () => {
+    console.log(operate(array[array.length-2], array[array.length-1], calculate));
+});
+
+
+// btn.forEach((button) => {
+//     button.addEventListener('click',(e) =>{
+//         content.textContent += e.target.textContent;
+//         displayedContent += e.target.textContent;
+//         output.appendChild(content);
+//     });
+// })
